e7358ba0b7ae04909d9fcd52801131adbe876820
Samuel Ortiz <sortizm5@miumg.edu.gt>
2020-10-24 00:02:43 -0600

    Se agrego la funcion para buscar una palabra por medio del correlativo.

--- Traductor.cpp
+++ Traductor.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
 #include <string>
 
 using namespace std;
 const char *nombre_archivo = "traductor.txt";
 
 struct Traductor{
+	int correlativo;
 	string palabra;
 	string Npalabra;
 	string traduccion;
 	string funcionalidad;
+	int bclave;
+	string linea;
+	string linea2;
+	string linea3;
+	int res;
 };
 
 void ingresar();
 void abrir();
+void buscar_palabra();
+void eliminar_palabra();
 
 main(){
 	int opcion;
@@ -22,8 +29,10 @@ main(){
 	while(true){
 		cout<<"1 - Agregar"<<endl;
 		cout<<"2 - Leer"<<endl;
-		cout<<"3 - Modificar"<<endl;
-		cout<<"4 - Eliminar"<<endl;
+		cout<<"3 - Buscar"<<endl;
+		cout<<"4 - Modificar"<<endl;
+		cout<<"5 - Eliminar"<<endl;
+		cout<<"6 - Salir"<<endl;
 		cout<<"Ingrese una opcion: ";
 		cin>>opcion;
 		
@@ -39,6 +48,22 @@ main(){
 					abrir();
 					break;
 				}
+			case 3:
+				{
+					buscar_palabra();
+					break;
+				}
+			case 4:
+				{
+					eliminar_palabra();
+					break;
+				}
+			case 6:
+				{
+					return 0;
+					break;
+				}
+			
 		}
 	}
 	return 0;
@@ -64,7 +89,7 @@ void abrir (){
 
 void ingresar(){
 	ofstream guardar;
-	guardar.open(nombre_archivo, ios::app); 
+	guardar.open(nombre_archivo, ios::app);
 
 	char s;
 	Traductor traductor;
@@ -74,6 +99,8 @@ void ingresar(){
 		exit(1);
 	}else{
 		do{
+			cout<<"Ingrese correlativo: ";
+			cin>>traductor.correlativo;
 			cout<<"Ingrese palabra: ";
 			cin>>traductor.palabra;
 			cout<<"Ingrese traduccion: ";
@@ -82,7 +109,7 @@ void ingresar(){
 			cin.ignore();
 			getline(cin, traductor.funcionalidad);
 			
-			guardar<<traductor.palabra<<", "<<traductor.traduccion<<", "<<traductor.funcionalidad<<endl;
+			guardar<<traductor.correlativo<<", "<<traductor.palabra<<", "<<traductor.traduccion<<", "<<traductor.funcionalidad<<endl;
 			
 			cout<<"Desea agregar mas datos (s/n)";
 			cin>>s;
@@ -92,4 +119,63 @@ void ingresar(){
 	
 	guardar.close();
 	//abrir();
+}
+
+void buscar_palabra(){
+	fstream buscar;
+	
+	Traductor traductor;
+	traductor.res = 0;
+	
+	cout<<"Ingrese el correlativo a buscar (formato: -#.): ";
+	cin>>traductor.Npalabra;
+	
+	if(!buscar.is_open()){
+		buscar.open(nombre_archivo, ios::in);
+	}
+	
+	while(getline(buscar, traductor.linea))
+	{
+		if(traductor.linea.find(traductor.Npalabra) != string::npos)
+		{
+			cout<<traductor.linea<<endl;
+			traductor.res = 1;
+		}
+	}
+	
+	if (traductor.res == 0){
+		cout<<traductor.Npalabra<<" no se encuentra"<<endl;
+	}
+}
+
+void eliminar_palabra(){
+	fstream buscar;
+	ofstream temp;
+	temp.open("Temp.txt");
+	
+	Traductor traductor;
+	traductor.res = 0;
+	
+	cout<<"Ingrese el correlativo a buscar (formato: -#.): ";
+	cin>>traductor.Npalabra;
+	
+	if(!buscar.is_open()){
+		buscar.open(nombre_archivo, ios::in);
+	}
+	
+	while(getline(buscar, traductor.linea))
+	{
+		if(traductor.linea.find(traductor.Npalabra) != string::npos)
+		{
+			cout<<traductor.linea<<endl;
+			cout<<"Palabra a eliminar"<<endl;
+			traductor.res = 1;
+		} else {
+			temp<<traductor.correlativo<<", "<<traductor.palabra<<", "<<traductor.traduccion<<", "<<traductor.funcionalidad<<endl;
+		}
+	}
+	
+	if (traductor.res == 0){
+		cout<<traductor.Npalabra<<" no se encuentra"<<endl;
+	}
 }
